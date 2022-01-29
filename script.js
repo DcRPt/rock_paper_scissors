baf61ed2e53ce4896d7587dd6f40430a7a53c087
DcRPt <76400183+DcRPt@users.noreply.github.com>
2022-01-29 01:28:50 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+const userChoiceDisplay = document.createElement("h1")
+const computerChoiceDisplay = document.createElement("h1")
+const resultDisplay = document.createElement("h1")
+const choice = document.getElementById("choice")
+const gamegrid = document.getElementById("game")
+choice.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+let userScore = 0
+let computerScore = 0
+
+const choices =["rock", "paper","scissors"]
+let userChoice
+
+const handleClick = (e) => {
+  userChoice = e.target.id
+  userChoiceDisplay.innerHTML = "User choice: " + userChoice
+  generateComputerChoice()
+  getResult()
+}
+
+const generateComputerChoice = () => {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+  computerChoice = randomChoice
+  computerChoiceDisplay.innerHTML = "Computer choice: " + computerChoice
+}
+
+for(let i = 0; i < choices.length; i++) {
+  const button = document.createElement("button")
+  button.id = choices[i]
+  button.innerHTML = choices[i]
+  button.addEventListener("click", handleClick)
+  gamegrid.appendChild(button)
+}
+
+function Score() {
+  yourScoreSpan.innerText = userScore
+  computerScoreSpan.innerText = computerScore
+}
+
+const getResult = () => {
+  switch (userChoice + computerChoice) {
+    case "scissorspaper":
+    case "rockscissors":
+    case "paperrock":
+      resultDisplay.innerHTML = userChoice + " beats " + computerChoice + ", YOU WIN!"
+      if( userScore < 5) {
+        userScore++
+        Score()
+      }
+      else {
+        userScore = 0
+        computerScore = 0
+      }
+      break
+    case "paperscissors":
+    case "scissorsrock":
+    case "rockpaper":
+      resultDisplay.innerHTML = computerChoice + " beats " + userChoice + ", YOU LOSE!"
+      if( computerScore < 5) {
+        computerScore++
+        Score()
+      }
+      else {
+        userScore = 0
+        computerScore = 0
+      }
+      break
+    case "paperpaper":
+    case "scisssorsscissors":
+    case "rockrock":
+      resultDisplay.innerHTML = "You chose " + userChoice + " and the computer chose " + computerChoice + ", ITS A DRAW!"
+      break
+  }
+}
